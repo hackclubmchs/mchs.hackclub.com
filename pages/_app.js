fb857cd7d362c31336f9ed336db87c1ccdf658d5
Ryan Luu <ryan.luu@ryanluu.dev>
2025-03-05 23:39:49 +0000

    Add meta
    Signed-off-by: GitHub <noreply@github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,22 @@ import NextApp from 'next/app'
 import theme from '@hackclub/theme'
 import { ThemeProvider } from 'theme-ui'
 import ColorSwitcher from '../components/color-switcher'
+import Head from 'next/head'
+import Meta from '@hackclub/meta'
 
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={theme}>
+        <Meta
+          as={Head}
+          name="Hack Club MCHS"
+          title="Home"
+          description="Welcome to Hack Club MCHS"
+          image="/card.png"
+          color="#5901c9"
+        />
         <ColorSwitcher />
         <Component {...pageProps} />
       </ThemeProvider>
