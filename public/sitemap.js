73a8b85ba1b6367a4d5d23fe5f14678a55205f65
Ryan Luu <ryan.luu@ryanluu.dev>
2025-03-06 17:37:10 +0000

    Add sitemap
    Signed-off-by: GitHub <noreply@github.com>

---
+++ public/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://mchs.hackclub.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ]
+}
