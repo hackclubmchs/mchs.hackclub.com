a723ce25f808f4b233011379d0403583a75f283e
Ryan Luu <ryan.luu@ryanluu.dev>
2025-03-07 22:30:56 +0000

    Reduce nesting
    Signed-off-by: GitHub <noreply@github.com>

--- components/footer.js
+++ components/footer.js
@@ -4,18 +4,14 @@ import { Container, Box } from 'theme-ui'
 export default () => (
   <Box
     as="footer"
-    sx={{ bg: 'sunken', textAlign: 'center', px: 2, py: [3, 4], mt: [4, 5] }}
+    sx={{
+      bg: 'sunken', textAlign: 'center', px: 2, py: [3, 4], mt: [4, 5],
+      'p': {
+        color: 'secondary',
+        a: { color: 'secondary' }
+      },
+    }}
   >
-    <Container
-      variant="narrow"
-      sx={{
-        'p': {
-          color: 'secondary',
-          a: { color: 'secondary' }
-        },
-      }}
-    >
-      <Footer />
-    </Container>
+    <Footer />
   </Box>
 )
