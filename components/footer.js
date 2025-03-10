e3ac2ee8ce45e0bfe09103ae18aca63455109277
Ryan Luu <ryan.luu@ryanluu.dev>
2025-03-10 22:28:48 +0000

    Add social icons to footer
    Signed-off-by: GitHub <noreply@github.com>

--- components/footer.js
+++ components/footer.js
@@ -1,17 +1,37 @@
 import Footer from './footer.mdx'
-import { Box } from 'theme-ui'
+import { Box, Grid, Link } from 'theme-ui'
+import Icon from './icon'
+
+const Service = ({ href, icon, ...props }) => (
+  <Link
+    target="_blank"
+    rel="noopener"
+    href={href}
+    title={`Hack Club MCHS on ${icon}`}
+    children={<Icon glyph={icon} />}
+    {...props}
+  />
+)
 
 const FooterComponent = () => (
   <Box
     as="footer"
     sx={{
+      color: 'secondary',
       bg: 'sunken', textAlign: 'center', px: 2, py: [3, 4], mt: [4, 5],
-      'p': {
-        color: 'secondary',
-        a: { color: 'secondary' }
-      },
+      a: { color: 'secondary' }
     }}
   >
+    <Box>
+      <Service
+        href="https://www.instagram.com/hackclubmchs"
+        icon="instagram"
+      />
+      <Service
+        href="https://github.com/hackclubmchs"
+        icon="github"
+      />
+    </Box>
     <Footer />
   </Box>
 );
