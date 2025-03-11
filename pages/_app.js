3a9eda4cbe5650bf4654b673e5830db0ce437719
Ryan Luu <ryan.luu@ryanluu.dev>
2025-03-11 08:39:41 +0000

    Cleanup code
    Signed-off-by: GitHub <noreply@github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,14 @@
+import { ThemeUIProvider } from 'theme-ui'
 import * as React from 'react'
 import NextApp from 'next/app'
-import Footer from '../components/footer'
-
-// import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '../lib/theme'
-import { ThemeUIProvider } from 'theme-ui'
-import ColorSwitcher from '../components/color-switcher'
-import Meta from '../components/meta'
-import Header from '../components/header'
 
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props
     return (
       <ThemeUIProvider theme={theme}>
-        <Meta />
-        <ColorSwitcher />
-        <Header />
         <Component {...pageProps} />
-        <Footer />
       </ThemeUIProvider>
     )
   }
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { ThemeUIProvider } from 'theme-ui'
+import Footer from '../components/footer'
+import Header from '../components/header'
+import Meta from '../components/meta'
+import theme from '../lib/theme'
+
+export default () => (
+  <>
+    <Meta />
+    <Header />
+    <Footer />
+  </>
+)
