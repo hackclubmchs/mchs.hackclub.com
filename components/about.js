2de8372915ed6829c03b002e89e53f3caf20e573
Ryan Luu <ryan.luu@ryanluu.dev>
2025-03-16 22:40:18 +0000

    Add basic about
    Signed-off-by: GitHub <noreply@github.com>

---
+++ components/about.js
@@ -0,0 +1,21 @@
+import { Container, Text } from 'theme-ui'
+
+export default () => (
+  <Container
+    variant="copy"
+    sx={{
+      my: 3,
+      py: 3,
+      border: '2px dashed',
+      borderColor: 'sunken',
+      borderRadius: 'default',
+      mt: [3, 4],
+      mb: [3, 4]
+    }}
+  >
+    <Text sx={{ fontSize: '24px' }}>
+      You can build anything, we want to show the potential of students. To
+      build confidence for education and future careers.
+    </Text>
+  </Container>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Footer from '../components/footer'
 import Header from '../components/header'
 import Meta from '../components/meta'
+import About from '../components/about'
 
 const HomePage = () => (
   <>
     <Meta />
     <Header />
+    <About />
     <Footer />
   </>
 )
